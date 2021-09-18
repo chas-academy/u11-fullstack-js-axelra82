922b8f3b88e28acabe22bd70b43af031c6494038
Axel <986301+axelra82@users.noreply.github.com>
2021-09-18 17:53:40 +0200

    feat: add bottom panel to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { LeftPanel, MainPanel, RightPanel } from './components/panels'
+import { LeftPanel, MainPanel, RightPanel, BottomPanel } from './components/panels'
 import './style/style.scss'
 
 const App = () => {
     return (
-        <>
+        <section id="app-container">
             <LeftPanel />
             <MainPanel />
             <RightPanel />
-        </>
+            <BottomPanel />
+        </section>
     )
 }
 
