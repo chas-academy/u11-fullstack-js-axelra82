799ce787be350633094f3ae98b380aa9a73b8251
Axel <986301+axelra82@users.noreply.github.com>
2021-09-18 20:53:50 +0200

    chore: add icon components

---
+++ src/components/icons/index.jsx
@@ -0,0 +1,6 @@
+import HomeIcon from './home'
+import MessagesIcon from './messages'
+import ProfileIcon from './profile'
+import SearchIcon from './search'
+
+export { HomeIcon, MessagesIcon, ProfileIcon, SearchIcon }
