ec92145b2f43ab73e281c35ea0deea4b5e8648b3
Axel <986301+axelra82@users.noreply.github.com>
2021-09-18 20:55:00 +0200

    fix: left panel items and correct responsive sizes

--- src/components/panels/left/index.jsx
+++ src/components/panels/left/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Logotype from '../../logotype'
+import LeftPanelMenu from './menu'
+import LeftPanelUser from './user'
 import './style.scss'
 
 const leftPanel = () => {
     return (
         <aside id="left-panel">
-            <Logotype /> LEFT PANEL
+            <Logotype />
+            <LeftPanelMenu />
+            <LeftPanelUser />
         </aside>
     )
 }
---
+++ src/components/panels/left/user/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.scss'
+
+const LeftPanelMenuComponent = () => {
+    return <section id="left-panel-user">Current user info</section>
+}
+
+export default LeftPanelMenuComponent
