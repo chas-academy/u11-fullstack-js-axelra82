b23e137238bbd0a3c36d7426e9bf8af6c6f97e23
Axel <986301+axelra82@users.noreply.github.com>
2021-09-22 14:56:52 +0200

    fix: change tos name and add container style

---
+++ src/pages/terms-of-service/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const TermsOfServicePage = () => {
+    return (
+        <section className="container">
+            <h1>Twitter Clone Terms of Service</h1>
+            <h2>
+                If you live outside the European Union, EFTA States, or the United Kingdom,
+                including if you live in the United States
+            </h2>
+            <p>
+                These Terms of Service (“Terms”) govern your access to and use of our services,
+                including our various websites, SMS, APIs, email notifications, applications,
+                buttons, widgets, ads, commerce services, and our{' '}
+                <a
+                    href="https://help.twitter.com/en/rules-and-policies/twitter-services-and-corporate-affiliates"
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                >
+                    other covered services
+                </a>{' '}
+                (
+                <a
+                    href="https://help.twitter.com/en/rules-and-policies/twitter-services-and-corporate-affiliates"
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                >
+                    https://help.twitter.com/en/rules-and-policies/twitter-services-and-corporate-affiliates
+                </a>
+                ) that link to these Terms (collectively, the “Services”), and any information,
+                text, links, graphics, photos, audio, videos, or other materials or arrangements of
+                materials uploaded, downloaded or appearing on the Services (collectively referred
+                to as “Content”). By using the Services you agree to be bound by these Terms.
+            </p>
+            <h3>NOTE!</h3>
+            <p>
+                This is a “terms placeholder” page for a school assignment and has no legal
+                standing. The content of this page is an excerpt from{' '}
+                <a href="https://twitter.com/en/tos">twitter.com/en/tos</a>
+            </p>
+        </section>
+    )
+}
+
+export default TermsOfServicePage
--- src/pages/user-agreement/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const UserAgreementPage = () => {
-    return <>USER AGREEMENT PAGE</>
-}
-
-export default UserAgreementPage
