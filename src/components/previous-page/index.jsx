de1d5e4b633149d5c0f2575a2ec82a186dbea081
Axel <986301+axelra82@users.noreply.github.com>
2021-09-22 15:35:14 +0200

    feat: create previous page component

---
+++ src/components/previous-page/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import ArrowLeftIconComponent from '../icons/arrow-left'
+import './style.scss'
+
+const PreviousPageComponent = () => {
+    const history = useHistory()
+
+    const previousPage = () => {
+        history.goBack()
+    }
+    return (
+        <section className="previous-page">
+            <button type="button" className="previous-page-button" onClick={previousPage}>
+                <ArrowLeftIconComponent />
+            </button>
+        </section>
+    )
+}
+export default PreviousPageComponent
