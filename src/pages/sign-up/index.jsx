2e081c2a0950063aa1bd1d400b6404c25af81448
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 11:11:02 +0200

    refactor: add bootstrap container

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import SignUpComponent from '../../components/signup'
 
 const SignUpPage = () => {
-    return <SignUpComponent />
+    return (
+        <Container className="d-flex align-items-center justify-content-center">
+            <section className="w-100 card-max-550">
+                <SignUpComponent />
+            </section>
+        </Container>
+    )
 }
 
 export default SignUpPage
