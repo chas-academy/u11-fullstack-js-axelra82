2134e1ac2debb26fda05ffc6326bf7a8e6d1dee6
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 13:43:42 +0200

    feat: create emoji component

---
+++ src/components/emoji/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const EmojiComponent = ({ emoji }) => {
+    return <span role="img">{emoji}</span>
+}
+
+export default EmojiComponent
