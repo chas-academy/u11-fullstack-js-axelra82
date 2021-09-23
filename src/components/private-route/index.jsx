eacae739dcb216d94fce622f249cd6a38d2b80b4
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 11:07:18 +0200

    refactor: move protected route to component
    Renam protected route to private route

---
+++ src/components/private-route/index.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import React, { useContext } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import StoreContext from '../../context/StoreContext'
+
+const PrivateRoute = ({ component: Page, path, ...rest }) => {
+    const {
+        store: { currentUser },
+    } = useContext(StoreContext)
+
+    return (
+        <Route
+            path={path}
+            {...rest}
+            render={(props) => {
+                return currentUser ? (
+                    <Page {...props} page={path} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/signup',
+                        }}
+                    />
+                )
+            }}
+        />
+    )
+}
+
+export default PrivateRoute
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useContext } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import StoreContext from './context/StoreContext'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import {
     SignUpPage,
     SignInPage,
@@ -12,35 +11,13 @@ import {
     CookiePolicyPage,
 } from './pages'
 import AppComponent from './layout/app'
+import PrivateRoute from './components/private-route'
 
 const PageRoutes = () => {
-    const { currentUser } = useContext(StoreContext)
-
-    // Protected routes for pages not accessible to guests
-    const ProtectedRoute = ({ component: Page, path, ...rest }) => {
-        return (
-            <Route
-                path={path}
-                {...rest}
-                render={(props) => {
-                    return currentUser ? (
-                        <Page {...props} page={path} />
-                    ) : (
-                        <Redirect
-                            to={{
-                                pathname: '/signup',
-                            }}
-                        />
-                    )
-                }}
-            />
-        )
-    }
-
     return (
         <Switch>
-            <ProtectedRoute exact path="/profile" component={AppComponent} />
-            <ProtectedRoute exact path="/messages" component={AppComponent} />
+            <PrivateRoute exact path="/profile" component={AppComponent} />
+            <PrivateRoute exact path="/messages" component={AppComponent} />
             <Route exact path="/search" component={AppComponent} />
             <Route exact path="/signup" component={SignUpPage} />
             <Route exact path="/signin" component={SignInPage} />
