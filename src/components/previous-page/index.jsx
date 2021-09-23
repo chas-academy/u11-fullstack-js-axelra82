05cdb16fadec3fef04a30933fb676b740235ae24
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 13:52:06 +0200

    refactor: remove classes prop

--- src/components/previous-page/index.jsx
+++ src/components/previous-page/index.jsx
@@ -3,15 +3,14 @@ import { useHistory } from 'react-router-dom'
 import ArrowLeftIconComponent from '../icons/arrow-left'
 import './style.scss'
 
-// eslint-disable-next-line react/prop-types
-const PreviousPageComponent = ({ classes = '' }) => {
+const PreviousPageComponent = () => {
     const history = useHistory()
 
     const previousPage = () => {
         history.goBack()
     }
     return (
-        <section className={`previous-page ${classes}`}>
+        <section id="previous-page">
             <button type="button" className="previous-page-button" onClick={previousPage}>
                 <ArrowLeftIconComponent />
             </button>
