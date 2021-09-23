fe9f5e32cc4e921556eb54f64b1f6444f8a3bfe9
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 13:00:06 +0200

    refactor: use sign in component on sign in page

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import SignInComponent from '../../components/signin'
 
 const SignInPage = () => {
-    return <>SIGNIN PAGE</>
+    return (
+        <Container className="d-flex flex-column align-items-center justify-content-center">
+            <section className="w-100 card-max-550">
+                <SignInComponent />
+            </section>
+        </Container>
+    )
 }
 
 export default SignInPage
