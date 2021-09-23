453d351ae9e4e6a36673e77ddc2b89aa55f24517
Axel <986301+axelra82@users.noreply.github.com>
2021-09-23 11:10:05 +0200

    refactor: add left menu user onclick for logout

--- src/layout/left/user/index.jsx
+++ src/layout/left/user/index.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import UserAvatar from '../../../components/user-avatar'
+import StoreContext from '../../../context/StoreContext'
 import './style.scss'
 
 const LeftUserComponent = () => {
-    const curreUserNamePlaceHolder = 'Axel Roussille Åberg'
+    const {
+        store: { currentUser, signout },
+    } = useContext(StoreContext)
+    const curreUserNamePlaceHolder = currentUser.displayName
     const curreUserHandlePlaceHolder = 'axelra82'
+    const [userActions, setUserActions] = useState(false)
+
+    const showActions = () => {
+        setUserActions(!userActions)
+    }
 
     return (
-        <section id="left-panel-user">
+        <section
+            id="left-panel-user"
+            onClick={showActions}
+            onKeyPress={showActions}
+            role="button"
+            tabIndex={0}
+        >
+            {userActions && (
+                <div id="user-actions">
+                    <div id="user-actions-current-user">
+                        <UserAvatar />
+                        <strong className="user-name">{curreUserNamePlaceHolder}</strong>
+                        <span className="user-handle text-muted">
+                            @{curreUserHandlePlaceHolder}
+                        </span>
+                    </div>
+                    <div
+                        id="user-actions-signout-user"
+                        onClick={() => signout()}
+                        onKeyPress={() => signout()}
+                        role="button"
+                        tabIndex={0}
+                    >
+                        logout
+                    </div>
+                </div>
+            )}
+
             <UserAvatar />
             <strong className="user-name">{curreUserNamePlaceHolder}</strong>
             <span className="user-handle text-muted">@{curreUserHandlePlaceHolder}</span>
