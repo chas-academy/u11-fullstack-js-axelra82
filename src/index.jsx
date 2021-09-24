4b42ce9ae86d94c89af1ea9f3db72585b6ca8215
Axel <986301+axelra82@users.noreply.github.com>
2021-09-24 21:22:33 +0200

    refactor: use new modal component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ModalComponent from './components/modal'
 import PageRoutes from './routes'
 import ContextProvider from './provider/ContextProvider'
 import './style/style.scss'
@@ -8,6 +9,7 @@ import './style/style.scss'
 ReactDOM.render(
     <BrowserRouter>
         <ContextProvider>
+            <ModalComponent />
             <PageRoutes />
         </ContextProvider>
     </BrowserRouter>,
