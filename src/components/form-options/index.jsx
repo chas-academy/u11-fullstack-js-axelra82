b896d8f584418bfd96f6972610332e75ce6b079a
Axel <986301+axelra82@users.noreply.github.com>
2021-09-24 21:38:12 +0200

    refactor!: breaking change
    Refactor to handle content in modal for route dependencies.

--- src/components/form-options/index.jsx
+++ src/components/form-options/index.jsx
@@ -1,19 +1,47 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import StoreContext from '../../context/StoreContext'
 
 const FormOptionsComponent = ({ options }) => {
+    const {
+        store: { modalState, toggleModal },
+    } = useContext(StoreContext)
+
+    const history = useHistory()
+
     const { action = '', route = '', message = '' } = options
 
+    const closeModalRedirect = (reroute) => {
+        toggleModal()
+        history.push(reroute)
+    }
+
     return (
-        <>
-            <div className="w-100 text-center text-size-medium mt-1">
-                {message} <Link to={`/${route}`}>{action}</Link>
-            </div>
-            <div className="w-100 text-center text-size-medium mt-1">
-                Continue without an account? <Link to="/">Browse</Link>
-            </div>
-        </>
+        <section className="mt-3">
+            <small className="d-block w-100 text-center mt-1">
+                {message}{' '}
+                {modalState ? (
+                    <Button variant="link" onClick={() => closeModalRedirect(`/${route}`)}>
+                        <small>{action}</small>
+                    </Button>
+                ) : (
+                    <Link to={`/${route}`}>{action}</Link>
+                )}
+            </small>
+
+            <small className="d-block w-100 text-center mt-1">
+                Continue without an account?{' '}
+                {modalState ? (
+                    <Button variant="link" onClick={() => closeModalRedirect('/')}>
+                        <small>Browse</small>
+                    </Button>
+                ) : (
+                    <Link to="/">Browse</Link>
+                )}
+            </small>
+        </section>
     )
 }
 
