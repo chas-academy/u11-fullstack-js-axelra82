4324fe65c75ff9e176a84dc3ee89673a96b5904a
Axel <986301+axelra82@users.noreply.github.com>
2021-09-24 21:40:01 +0200

    feat: start work on user profile update option

--- src/views/profile/index.jsx
+++ src/views/profile/index.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from 'react-bootstrap'
+import UpdateUserProfile from '../../components/user/update'
+import StoreContext from '../../context/StoreContext'
 
 const ProfileView = () => {
-    return <>PROFILE VIEW</>
+    const {
+        store: { isSignUp, currentUser, toggleModal, modalContent, setModalContent },
+    } = useContext(StoreContext)
+
+    const { name, email, bio, dob } = currentUser
+
+    console.log(name, email, bio, dob)
+
+    console.log('------- KNOW IF WE JUST SIGNED UP -------')
+    console.log(isSignUp)
+
+    const showEditProfile = () => {
+        setModalContent({
+            ...modalContent,
+            header: {
+                ...modalContent.header,
+                show: true,
+                title: 'Edit profile',
+            },
+            body: <UpdateUserProfile />,
+            footer: {
+                ...modalContent.footer,
+                show: true,
+                buttons: [
+                    {
+                        text: 'update',
+                    },
+                ],
+            },
+        })
+        toggleModal()
+    }
+
+    return (
+        <>
+            <h1>{name}</h1>
+            <Button
+                onClick={() => showEditProfile()}
+                className="rounded-pill"
+                variant="outline-secondary"
+            >
+                Edit profile
+            </Button>
+        </>
+    )
 }
 
 export default ProfileView
