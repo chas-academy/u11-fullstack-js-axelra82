d63ad221dfd878dbeb6a9b3f277ad919c676457d
Axel <986301+axelra82@users.noreply.github.com>
2021-09-24 21:33:26 +0200

    refactor!: breaking change
    Refactor component to include form only.
    This will allow for more versatile use of component.

--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -1,76 +1,71 @@
 /* eslint-disable no-console */
-import React, { useRef, useState, useContext } from 'react'
+import React, { useRef, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import PreviousPage from '../previous-page'
+import { Form, Button } from 'react-bootstrap'
+
 import FormOptions from '../form-options'
 import StoreContext from '../../context/StoreContext'
 
 const SignInComponent = () => {
     const {
-        store: { signin },
+        store: { loading, setLoading, signin, modalState, toggleModal },
     } = useContext(StoreContext)
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const [submitError, setsubmitError] = useState('')
-    const [loading, setLoading] = useState(false)
+    // const [submitError, setsubmitError] = useState('')
     const history = useHistory()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         try {
-            setsubmitError('')
+            // setsubmitError('')
             setLoading(true)
             await signin(emailRef.current.value, passwordRef.current.value)
-            history.push('/')
+            if (modalState) {
+                toggleModal()
+            } else {
+                history.push('/')
+            }
         } catch (catchError) {
             console.log(catchError)
-            setsubmitError('Failed to login')
+            // setsubmitError('Failed to login')
         }
 
         return setLoading(false)
     }
 
+    // {submitError && <Alert variant="danger">{submitError}</Alert>}
+
     return (
         <>
-            <Card className="mt-2">
-                <PreviousPage />
-                <Card.Body>
-                    {submitError && <Alert variant="danger">{submitError}</Alert>}
-                    <h1 className="text-center">Sign In</h1>
-                    <p>
-                        Sign in to your account and start sharing your thoughts with the world
-                        instantly
-                    </p>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email" className="mt-1">
-                            <Form.Label className="text-muted">Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                ref={emailRef}
-                                required
-                                placeholder="you@domain.tld"
-                                aria-placeholder="you@domain.tld"
-                            />
-                        </Form.Group>
-                        <Form.Group id="password" className="mt-1">
-                            <Form.Label className="text-muted">Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                ref={passwordRef}
-                                required
-                                placeholder="minimum 6 character"
-                                aria-placeholder="minimum 6 character"
-                            />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100 mt-2" type="submit">
-                            Sign In
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group id="email" className="mt-1">
+                    <Form.Label className="text-muted">Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        ref={emailRef}
+                        required
+                        placeholder="your@email.com"
+                        aria-placeholder="your@email.com"
+                    />
+                </Form.Group>
+                <Form.Group id="password" className="mt-1">
+                    <Form.Label className="text-muted">Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        ref={passwordRef}
+                        required
+                        placeholder="minimum 6 character"
+                        aria-placeholder="minimum 6 character"
+                    />
+                </Form.Group>
+                <Button disabled={loading} className="w-100 mt-3" type="submit">
+                    Sign In
+                </Button>
+            </Form>
+
             <FormOptions
                 options={{ action: 'Sign Up', route: 'signup', message: 'Need an account?' }}
             />
