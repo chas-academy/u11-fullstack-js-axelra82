4c760304e82cd4dfe75484455f38bbf6f6d0ea4a
Axel <986301+axelra82@users.noreply.github.com>
2021-09-24 21:33:56 +0200

    refactor: use refactored sign in component with bootstrap card

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Card } from 'react-bootstrap'
 import SignInComponent from '../../components/signin'
+import PreviousPage from '../../components/previous-page'
 
 const SignInPage = () => {
     return (
         <Container className="d-flex flex-column align-items-center justify-content-center">
             <section className="w-100 card-max-550">
-                <SignInComponent />
+                <Card className="mt-2">
+                    <PreviousPage />
+                    <Card.Body>
+                        <h1 className="text-center">Sign In</h1>
+                        <p>
+                            Sign in to your account and start sharing your thoughts with the world
+                            instantly
+                        </p>
+                        <SignInComponent />
+                    </Card.Body>
+                </Card>
             </section>
         </Container>
     )
