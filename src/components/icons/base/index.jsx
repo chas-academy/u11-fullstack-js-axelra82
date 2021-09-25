fed1a83222b9385c6153baa2d1799d5136cc10c6
Axel <986301+axelra82@users.noreply.github.com>
2021-09-25 13:36:54 +0200

    refactor: add box view size option with default

--- src/components/icons/base/index.jsx
+++ src/components/icons/base/index.jsx
@@ -1,12 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
-// eslint-disable-next-line react/prop-types
-const BaseIconComponent = ({ graphic = null, color = 'black', size = '', classes = '' }) => {
+const BaseIconComponent = ({
+    graphic = null,
+    color = 'black',
+    size = '',
+    classes = '',
+    box = '0 0 24 24',
+}) => {
     return (
         <svg
             className={`custom-icon ${size !== '' ? `icon-${size}` : ''} fill-${color} ${classes}`}
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+            viewBox={box}
         >
             {graphic}
         </svg>
