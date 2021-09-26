c5f320875c8e859b69d630330af026df7fc66344
Axel <986301+axelra82@users.noreply.github.com>
2021-09-26 19:30:56 +0200

    fix: allow dynamic handle routes in root

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useLocation } from 'react-router-dom'
 import {
     SignUpPage,
     SignInPage,
@@ -14,9 +14,12 @@ import AppComponent from './layout/app'
 import PrivateRoute from './components/private-route'
 
 const PageRoutes = () => {
+    const location = useLocation()
+    const { state } = location
+
     return (
         <Switch>
-            <PrivateRoute exact path="/profile" component={AppComponent} />
+            {/* <PrivateRoute exact path="/profile" component={AppComponent} /> */}
             <PrivateRoute exact path="/messages" component={AppComponent} />
             <Route exact path="/search" component={AppComponent} />
             <Route exact path="/signup" component={SignUpPage} />
@@ -24,8 +27,8 @@ const PageRoutes = () => {
             <Route exact path="/terms" component={TermsOfServicePage} />
             <Route exact path="/integrity-policy" component={IntegrityPloicyPage} />
             <Route exact path="/cookie-policy" component={CookiePolicyPage} />
-            <Route exact path="/" component={AppComponent} />
-            <Route component={NotFound} />
+            <Route exact path="/:handle" component={AppComponent} />
+            <Route component={state === '404' ? NotFound : AppComponent} />
         </Switch>
     )
 }
