f3831479bff6d427a179310d485726eda6c5719c
Axel <986301+axelra82@users.noreply.github.com>
2021-09-26 19:42:49 +0200

    refactor: use sign up form component in sign up page

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Card } from 'react-bootstrap'
 import SignUpComponent from '../../components/signup'
+import PreviousPage from '../../components/previous-page'
 
 const SignUpPage = () => {
     return (
-        <Container className="d-flex align-items-center justify-content-center">
-            <section className="w-100 card-max-550">
-                <SignUpComponent />
-            </section>
+        <Container className="d-flex w-100 mt-5 align-content-center justify-content-center">
+            <Card as="article" className="card-max-550">
+                <PreviousPage classes="mt-1" />
+                <Card.Body>
+                    <h1 className="text-center">Sign Up</h1>
+                    <p>
+                        Create an account and start sharing your thoughts with the world instantly
+                    </p>
+                    <SignUpComponent />
+                </Card.Body>
+            </Card>
         </Container>
     )
 }
