49ff36bbff735416b2081c589443341e2ca269e6
Axel <986301+axelra82@users.noreply.github.com>
2021-09-26 19:36:13 +0200

    fix: handle back button for 404 and loop issue

--- src/components/previous-page/index.jsx
+++ src/components/previous-page/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 import ArrowLeftIconComponent from '../icons/arrow-left'
 import './style.scss'
 
 const PreviousPageComponent = () => {
     const history = useHistory()
+    const location = useLocation()
 
     const previousPage = () => {
         const { action } = history
+        const { state } = location
 
         // check if we can pop or push
         // pop means there is nothing to push to
         // push means there is history for go back function
-        if (action === 'POP') {
-            history.push('/')
+        // in both scenarios we reset state to avoid loops
+        if (action === 'POP' || state === '404') {
+            history.push('/', '')
         } else {
+            location.state = ''
             history.goBack()
         }
     }
