8491bcb8f50138e8f3b20cd18a9896f6c5602fb2
Axel <986301+axelra82@users.noreply.github.com>
2021-09-26 19:42:16 +0200

    refactor: handle signed in user scenario

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Container, Card } from 'react-bootstrap'
 import SignInComponent from '../../components/signin'
 import PreviousPage from '../../components/previous-page'
+import StoreContext from '../../context/StoreContext'
 
 const SignInPage = () => {
+    const { store: currentUser } = useContext(StoreContext)
+    const history = useHistory()
+
+    useEffect(() => {
+        if (currentUser) {
+            history.push('/')
+        }
+    }, [currentUser])
+
     return (
-        <Container className="d-flex flex-column align-items-center justify-content-center">
-            <section className="w-100 card-max-550">
-                <Card className="mt-2">
-                    <PreviousPage />
-                    <Card.Body>
-                        <h1 className="text-center">Sign In</h1>
-                        <p>
-                            Sign in to your account and start sharing your thoughts with the world
-                            instantly
-                        </p>
-                        <SignInComponent />
-                    </Card.Body>
-                </Card>
-            </section>
+        <Container className="d-flex w-100 mt-5 align-content-center justify-content-center">
+            <Card as="article" className="card-max-550">
+                <PreviousPage classes="mt-1" />
+                <Card.Body>
+                    <h1 className="text-center">Sign In</h1>
+                    <p>
+                        Sign in to your account and start sharing your thoughts with the world
+                        instantly
+                    </p>
+                    <SignInComponent />
+                </Card.Body>
+            </Card>
         </Container>
     )
 }
