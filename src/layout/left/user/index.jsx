d895daaa2888c5ad9373f1ea0d6c17102c91f577
Axel <986301+axelra82@users.noreply.github.com>
2021-09-27 16:23:33 +0200

    refactor!: breaking change
    Refactor to use bootstrap classes and popover. Delete style reference

--- src/layout/left/user/index.jsx
+++ src/layout/left/user/index.jsx
@@ -1,59 +1,80 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useContext } from 'react'
+import { Row, Col, Popover, OverlayTrigger, Button } from 'react-bootstrap'
 import UserAvatar from '../../../components/user/avatar'
+import { CheckmarkIcon, HorizontalDotsIcon } from '../../../components/icons'
 import StoreContext from '../../../context/StoreContext'
-import './style.scss'
 
 const LeftUserComponent = () => {
     const {
         store: { currentUser, signout },
     } = useContext(StoreContext)
-    const curreUserNamePlaceHolder = currentUser.name
-    const curreUserHandlePlaceHolder = currentUser.handle
-    const [userActions, setUserActions] = useState(false)
+    const curreUserName = currentUser.name
+    const curreUserHandle = currentUser.handle
+    const [isActive, setIsActive] = useState(false)
 
-    const showActions = () => {
-        setUserActions(!userActions)
+    const popoverActive = () => {
+        setIsActive(!isActive)
     }
 
-    const UserDisplayComponent = () => {
+    const UserDisplayComponent = ({ popover = false }) => {
         return (
-            <>
-                <UserAvatar fullWidth />
-                <small>
-                    <strong className="user-name">{curreUserNamePlaceHolder}</strong>
-                </small>
-                <small className="user-handle text-muted">@{curreUserHandlePlaceHolder}</small>
-            </>
+            <Row className="gx-3">
+                <Col xs={3}>
+                    <UserAvatar />
+                </Col>
+                <Col xs={8} className="d-flex flex-column">
+                    <small>
+                        <strong className="user-name">{curreUserName}</strong>
+                    </small>
+                    <small className="user-handle text-muted lh-1">@{curreUserHandle}</small>
+                </Col>
+                {popover ? (
+                    <Col xs={1} className="d-flex justify-content-center align-items-center p-0">
+                        <CheckmarkIcon />
+                    </Col>
+                ) : (
+                    <Col xs={1} className="d-flex justify-content-center align-items-center p-0">
+                        <HorizontalDotsIcon />
+                    </Col>
+                )}
+            </Row>
         )
     }
 
+    const popover = (
+        <Popover id="user-popover">
+            <Popover.Body className="px-3 pe-4">
+                <UserDisplayComponent popover />
+            </Popover.Body>
+            <div className="border-bottom" />
+            <Button
+                onClick={() => signout()}
+                className="my-2 px-3 py-2 w-100 text-start bg-extra-light-gray-hover rounded-0 text-black"
+                variant="link"
+            >
+                <small>sign out @{curreUserHandle}</small>
+            </Button>
+        </Popover>
+    )
+
     return (
-        <section
-            id="left-panel-user"
-            className="ps-2 py-2 mt-auto mb-2 rounded-pill"
-            onClick={showActions}
-            onKeyPress={showActions}
-            role="button"
-            tabIndex={0}
-        >
-            {userActions && (
-                <div id="user-actions">
-                    <div id="user-actions-current-user">
-                        <UserDisplayComponent />
-                    </div>
-                    <div
-                        id="user-actions-signout-user"
-                        onClick={() => signout()}
-                        onKeyPress={() => signout()}
-                        role="button"
-                        tabIndex={0}
-                    >
-                        sign out
-                    </div>
-                </div>
-            )}
-            <UserDisplayComponent />
-        </section>
+        <>
+            <OverlayTrigger
+                onToggle={popoverActive}
+                trigger="click"
+                placement="top-start"
+                overlay={popover}
+            >
+                <section
+                    className={`mt-auto mb-3 p-2 pe-4 rounded-pill ${
+                        isActive ? '' : 'bg-extra-light-gray-hover curser-pointer'
+                    }`}
+                >
+                    <UserDisplayComponent />
+                </section>
+            </OverlayTrigger>
+        </>
     )
 }
 
