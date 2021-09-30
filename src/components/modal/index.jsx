f32ab6baf9be36d34b9f1a52bbaf7f97da5108b8
Axel <986301+axelra82@users.noreply.github.com>
2021-09-30 10:27:50 +0200

    refactor: add span wrapper with classes option
    Primarily used to limit model contet height

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -7,38 +7,40 @@ const ModalComponent = () => {
         store: { modalState, toggleModal, modalContent },
     } = useContext(StoreContext)
 
-    const { header, body, footer } = modalContent
+    const { classes, header, body, footer } = modalContent
     const { show: showHeader, title, content: headerContent } = header
     const { show: showFooter, buttons } = footer
 
     return (
         <>
             <Modal show={modalState} onHide={toggleModal} centered fullscreen="md-down">
-                {showHeader && (
-                    <Modal.Header closeButton>
-                        {title && <Modal.Title>{title}</Modal.Title>}
-                        {headerContent}
-                    </Modal.Header>
-                )}
-                {body !== '' && <Modal.Body>{body}</Modal.Body>}
-                {showFooter && (
-                    <Modal.Footer>
-                        {buttons.length &&
-                            buttons.map((button, index) => {
-                                const { variant, text } = button
-                                return (
-                                    <Button
-                                        // eslint-disable-next-line react/no-array-index-key
-                                        key={`modal-footer-button-${index}`}
-                                        variant={variant}
-                                        onClick={toggleModal}
-                                    >
-                                        {text}
-                                    </Button>
-                                )
-                            })}
-                    </Modal.Footer>
-                )}
+                <span className={classes}>
+                    {showHeader && (
+                        <Modal.Header closeButton>
+                            {title && <Modal.Title>{title}</Modal.Title>}
+                            {headerContent}
+                        </Modal.Header>
+                    )}
+                    {body !== '' && <Modal.Body>{body}</Modal.Body>}
+                    {showFooter && (
+                        <Modal.Footer>
+                            {buttons.length &&
+                                buttons.map((button, index) => {
+                                    const { variant, text, action = null } = button
+                                    return (
+                                        <Button
+                                            // eslint-disable-next-line react/no-array-index-key
+                                            key={`modal-footer-button-${index}`}
+                                            variant={variant}
+                                            onClick={action}
+                                        >
+                                            {text}
+                                        </Button>
+                                    )
+                                })}
+                        </Modal.Footer>
+                    )}
+                </span>
             </Modal>
         </>
     )
