47e46311aece5f071bc00a44cfafabed2e831bea
Axel <986301+axelra82@users.noreply.github.com>
2021-09-30 21:59:58 +0200

    refactor!: breaking changes
    Major refactor of context. Overall cleaner display element functions and states

--- src/provider/ContextProvider.jsx
+++ src/provider/ContextProvider.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
@@ -5,7 +6,7 @@
 // pass detached states
 
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { initializeApp } from 'firebase/app'
 import {
     getAuth,
@@ -13,6 +14,9 @@ import {
     signInWithEmailAndPassword,
     onAuthStateChanged,
     updateProfile,
+    sendPasswordResetEmail,
+    getAdditionalUserInfo,
+    deleteUser,
 } from 'firebase/auth'
 import {
     getFirestore,
@@ -23,34 +27,33 @@ import {
     getDocs,
     getDoc,
     setDoc,
-    Timestamp,
+    deleteDoc,
+    updateDoc,
 } from 'firebase/firestore'
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { dateFunctions, regexFunctions } from '../helper-functions'
 import Context from '../context/StoreContext'
 
 const StoreContext = ({ children }) => {
     const defaultModalContent = {
-        header: { show: false, title: '', content: '' },
-        body: '',
+        classes: null,
+        actions: null,
+        header: { show: false, title: null, content: null },
+        body: null,
         footer: { show: false, buttons: [] },
     }
 
+    const { isoDateString, formatDateString } = dateFunctions
+    const { urlReplace } = regexFunctions
+
     const history = useHistory()
+    const location = useLocation()
+    const { state: locationState } = location
     const [currentUser, setCurrentUser] = useState()
     const [loading, setLoading] = useState(true)
     const [toasts, setToasts] = useState([])
     const [modalState, setModalState] = useState(false)
-    const [isSignUp, setIsSignUp] = useState(false)
     const [modalContent, setModalContent] = useState(defaultModalContent)
-
-    const toggleModal = () => {
-        setModalState(!modalState)
-        // use timeout to make content transition more pleasing
-        setTimeout(() => {
-            // we need to reset the modal content for future use
-            // eslint-disable-next-line no-unused-expressions
-            modalState && setModalContent(defaultModalContent)
-        }, 350)
-    }
 
     const app = initializeApp({
         apiKey: process.env.REACT_APP_API_KEY,
@@ -64,6 +67,35 @@ const StoreContext = ({ children }) => {
     const auth = getAuth()
     const db = getFirestore(app)
 
+    const toggleModal = (withTimeOut = true) => {
+        setModalState(!modalState)
+        // use timeout to make content transition more pleasing
+        if (withTimeOut) {
+            setTimeout(() => {
+                // we need to reset the modal content for future use
+                // eslint-disable-next-line no-unused-expressions
+                modalState && setModalContent(defaultModalContent)
+            }, 350)
+        } else {
+            setModalContent(defaultModalContent)
+        }
+    }
+
+    const toastCatchError = (errorMessage) => {
+        const messageString =
+            typeof errorMessage === 'string'
+                ? errorMessage
+                : errorMessage.message.replace(/.*\/((.*)\))/gi, '$2').replace(/-/gi, ' ')
+        return setToasts([
+            ...toasts,
+            {
+                header: 'Error',
+                body: messageString,
+                variant: 'danger',
+            },
+        ])
+    }
+
     const usernameLookUp = (table, username) => {
         // look for users where username matches
         const getTable = collection(db, table)
@@ -71,7 +103,7 @@ const StoreContext = ({ children }) => {
         return getDocs(usernameMatch)
     }
 
-    // always make sure username is unique
+    // make sure username is unique
     const usernameCheck = async (string) => {
         // make sure string only contains lower, uppercase letters and numbers
         let safeUsername = string
@@ -101,23 +133,36 @@ const StoreContext = ({ children }) => {
         return safeUsername
     }
 
-    const signup = async (dob, email, nameFirst, nameLast, password, profilePicture, username) => {
-        await createUserWithEmailAndPassword(auth, email, password)
+    const signup = async (dob, email, username, nameFirst, nameLast, password) => {
+        // create user
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        // update user display name
         await updateProfile(auth.currentUser, { displayName: `${nameFirst} ${nameLast}` })
+
+        // create initial random profile picture theme
+        const randomNumber = Math.floor(Math.random() * 5 + 1)
+        const randomTheme = `theme${randomNumber}`
+
+        // create user db entry
         await setDoc(doc(db, 'users', auth.currentUser.uid), {
             bio: '',
-            dob: Timestamp.fromDate(new Date(dob)),
+            dob: formatDateString(new Date(dob)),
             email,
-            website: '',
+            joined: formatDateString(new Date()),
             name: { first: nameFirst, last: nameLast },
-            profilePicture,
+            profilePicture: randomTheme,
             username,
-            user_type: doc(db, 'user_types', 'registered'),
+            userType: doc(db, 'user_types', 'registered'),
+            website: '',
         })
+        // return additional user details
+        const response = getAdditionalUserInfo(userCredentials)
+        return response
     }
 
-    const signin = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
+    const signin = async (email, password) => {
+        const response = await signInWithEmailAndPassword(auth, email, password)
+        return response
     }
 
     const signout = () => {
@@ -125,23 +170,40 @@ const StoreContext = ({ children }) => {
         history.push('/signin')
     }
 
-    const resetPassword = (email) => {
-        return auth.sendPasswordResetEmail(email)
+    const resetPassword = async (email) => {
+        await sendPasswordResetEmail(auth, email)
     }
 
     const updatePassword = (password) => {
         return currentUser.updatePassword(password)
     }
 
-    const updateProfilePicture = (image) => {
-        return 'ok'
+    const updateProfilePicture = async (username, fileBlob, fileType) => {
+        const fileLocation = `users/${auth.currentUser.uid}/profile-picture/${username}.${fileType}`
+        // root storage reference
+        const storage = getStorage()
+
+        // creat image reference
+        const imageRef = ref(storage, fileLocation)
+
+        // await file upload
+        const storageRef = await uploadBytes(imageRef, fileBlob)
+        const publicFile = await getDownloadURL(storageRef.metadata.ref)
+
+        // update auth user meta data
+        await updateProfile(auth.currentUser, { photoURL: publicFile })
+
+        // update user db profile picture entry
+        await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+            profilePicture: publicFile,
+        })
     }
 
-    const getPublicProfile = async (publicProfileUsername) => {
+    const getProfileData = async (profileUsername) => {
         setLoading(true)
 
         let publicProfile = null
-        const querySnapshot = await usernameLookUp('users', publicProfileUsername)
+        const querySnapshot = await usernameLookUp('users', profileUsername)
 
         if (querySnapshot.docs.length > 0) {
             querySnapshot.forEach((docMatch) => {
@@ -154,44 +216,49 @@ const StoreContext = ({ children }) => {
         return publicProfile
     }
 
+    const deleteProfile = async (password) => {
+        try {
+            // get fresh user credentials
+            await signin(auth.currentUser.email, password)
+            // delete user db entries
+            await deleteDoc(doc(db, 'users', auth.currentUser.uid))
+
+            // move to home view and reset modal
+            history.push('/')
+            toggleModal(false)
+
+            // delete user from auth
+            await deleteUser(auth.currentUser)
+        } catch (catchError) {
+            toastCatchError(catchError)
+        }
+    }
+
+    const getCurrentUserDbEntry = async (user) => {
+        const userDbDoc = doc(db, 'users', user.uid)
+        const userDoc = await getDoc(userDbDoc)
+
+        if (userDoc.exists()) {
+            const userData = userDoc.data()
+            setCurrentUser(userData)
+        } else if (
+            typeof locationState !== 'undefined' &&
+            typeof locationState.isNew !== 'undefined' &&
+            locationState.isNew
+        ) {
+            getCurrentUserDbEntry(user)
+        } else {
+            toastCatchError(
+                'Could not find user in database. Please reload page. If the problem persists please contact site administrator'
+            )
+        }
+    }
+
     useEffect(() => {
         setLoading(true)
         onAuthStateChanged(auth, async (user) => {
             if (user) {
-                const userDbDoc = doc(db, 'users', user.uid)
-                const userDoc = await getDoc(userDbDoc)
-
-                if (userDoc.exists()) {
-                    const userData = userDoc.data()
-                    const {
-                        bio,
-                        dob,
-                        email,
-                        name: { first, last },
-                        profilePicture,
-                        username,
-                        website,
-                    } = userData
-
-                    setCurrentUser({
-                        bio,
-                        dob,
-                        email,
-                        name: `${first} ${last}`,
-                        profilePicture,
-                        username,
-                        website,
-                    })
-                } else {
-                    setToasts([
-                        ...toasts,
-                        {
-                            header: 'Error',
-                            body: 'Could not find user in database. Please reload page. If the problem persists please contact site administrator',
-                            variant: 'danger',
-                        },
-                    ])
-                }
+                getCurrentUserDbEntry(user)
             } else {
                 setCurrentUser(user)
             }
@@ -203,6 +270,8 @@ const StoreContext = ({ children }) => {
     const store = {
         loading,
         setLoading,
+        formatDateString,
+        isoDateString,
         toasts,
         setToasts,
         modalState,
@@ -210,8 +279,6 @@ const StoreContext = ({ children }) => {
         modalContent,
         setModalContent,
         toggleModal,
-        isSignUp,
-        setIsSignUp,
         currentUser,
         usernameCheck,
         signup,
@@ -219,7 +286,11 @@ const StoreContext = ({ children }) => {
         signout,
         resetPassword,
         updatePassword,
-        getPublicProfile,
+        getProfileData,
+        updateProfilePicture,
+        deleteProfile,
+        toastCatchError,
+        urlReplace,
     }
 
     return <Context.Provider value={{ store }}>{children}</Context.Provider>
