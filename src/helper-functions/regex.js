65ff81c7c10dfc911a5b916ed7480cc2c849354e
Axel <986301+axelra82@users.noreply.github.com>
2021-10-04 11:40:43 +0200

    feat: add url check regex

--- src/helper-functions/regex.js
+++ src/helper-functions/regex.js
@@ -1,7 +1,15 @@
-/* eslint-disable import/prefer-default-export */
 const urlReplace = (string) => {
     const regEx = /^((https:\/\/)|(http:\/\/)|(www)\.)(\S*)/gi
     return string.replace(regEx, '$5')
 }
 
-export { urlReplace }
+const urlCheck = (string) => {
+    const regEx = /^((https:\/\/)|(http:\/\/)|(www)\.)(\S*)/gi
+    const hasProtocol = string.match(regEx)
+    if (!hasProtocol) {
+        return `https://${string}`
+    }
+    return string
+}
+
+export { urlReplace, urlCheck }
