c4c4acda1b6262fed263f449b247b19966337d99
Axel <986301+axelra82@users.noreply.github.com>
2021-10-07 21:04:27 +0200

    refactor: add content to home view

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,14 +1,39 @@
-import React, { useContext } from 'react'
-import StoreContext from '../../context/StoreContext'
+/* eslint-disable no-undef */
+import React, { useContext, useState, useEffect } from 'react'
 import CreatPostComponent from '../../components/create-post'
+import PostListComponent from '../../components/post-list'
+import StoreContext from '../../context/StoreContext'
 
 const HomeView = () => {
-    const { currentUser } = useContext(StoreContext)
+    const {
+        store: { currentUser, setPosts, setPostActiontoggle },
+    } = useContext(StoreContext)
+
+    const [loading, setLoading] = useState()
+
+    useEffect(() => {
+        setLoading(true)
+        ;(async () => {
+            const getPosts = await fetch(`${process.env.REACT_APP_WEB_API}/posts/list`)
+            if (getPosts.status === 200) {
+                const postsJson = await getPosts.json()
+                setPosts(postsJson)
+                setPostActiontoggle(
+                    postsJson.map((_, index) => {
+                        return { id: index, state: false }
+                    })
+                )
+            }
+            setLoading(false)
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <>
-            {currentUser && <CreatPostComponent />}
-            PUBLIC POSTS LIST
+            {currentUser && <CreatPostComponent classes="border-bottom" />}
+
+            <PostListComponent loading={loading} />
         </>
     )
 }
