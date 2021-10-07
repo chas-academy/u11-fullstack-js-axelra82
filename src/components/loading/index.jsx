12901a07fa87d7f440c81f54cefe5dc1dff6d73b
Axel <986301+axelra82@users.noreply.github.com>
2021-10-07 20:50:49 +0200

    refactor: add text option

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import { Spinner } from 'react-bootstrap'
 
-const LoadingComponent = () => {
+const LoadingComponent = ({ messageTop = false, messageBottom = false }) => {
     return (
-        <section className="d-flex min-vh-100 align-items-center justify-content-center p-3">
+        <section className="d-flex flex-column align-items-center justify-content-center py-5">
+            <div className="my-3 w-100 text-center">{messageTop}</div>
             <Spinner animation="grow" variant="primary" />
+            <div className="my-3 w-100 text-center">{messageBottom}</div>
         </section>
     )
 }
