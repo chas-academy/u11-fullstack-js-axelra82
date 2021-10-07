613a6afa87e12e892a3aa3dfc905f085d9e18668
Axel <986301+axelra82@users.noreply.github.com>
2021-10-07 21:01:14 +0200

    refactor: add is admin check

--- src/helper-functions/firebase/user-object-format.js
+++ src/helper-functions/firebase/user-object-format.js
@@ -1,13 +1,21 @@
 import { getDoc } from 'firebase/firestore'
 import { formatDateString } from '../dates'
 
-const userObjectFormat = async (data) => {
-    const userDate = data
-    const roleDoc = await getDoc(userDate.role)
-    userDate.role = roleDoc.id
-    userDate.dob = formatDateString(userDate.dob.toDate())
-    userDate.joined = formatDateString(userDate.joined.toDate(), 'short')
-    return userDate
+const userObjectFormat = async (data, setUser, setIsAdmin = false) => {
+    const userData = data
+    const roleDoc = await getDoc(userData.role)
+    userData.role = roleDoc.id
+    userData.dob = formatDateString(userData.dob.toDate())
+    userData.joined = formatDateString(userData.joined.toDate(), 'short')
+    // check if user is administrator
+    if (setIsAdmin && userData.role === 'administrator') {
+        setIsAdmin(true)
+    }
+    // if user is authenticated add uid
+    if (typeof data.uid !== 'undefined') {
+        userData.uid = data.uid
+    }
+    setUser(userData)
 }
 
 export default userObjectFormat
