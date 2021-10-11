194f35b8ea1b2f4de4473ce547e1cb4f0bd08376
Axel <986301+axelra82@users.noreply.github.com>
2021-10-11 08:14:56 +0200

    fix: minor hotfix
    Add a debouncer to search as to not trigger API request on every keystroke.

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import algoliasearch from 'algoliasearch/lite'
+import { Spinner } from 'react-bootstrap'
 import SearchIcon from '../icons/search'
 
 const SearchComponent = () => {
@@ -9,21 +10,35 @@ const SearchComponent = () => {
     const apiKey = process.env.REACT_APP_ALGOLIA_API_KEY
     const searchClient = algoliasearch(appId, apiKey)
     const index = searchClient.initIndex('instant_search')
-    const [hasContent, setHasContent] = useState(false)
+
+    const [isSearch, setIsSearch] = useState(false)
     const [hits, setHits] = useState([])
+    const [hasContent, setHasContent] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
+
+    // custom debounce hook
+    const useDebounce = (value, delay) => {
+        const [debouncedValue, setDebouncedValue] = useState(value)
+        useEffect(() => {
+            setIsSearch(true)
+            const handler = setTimeout(() => {
+                setDebouncedValue(value)
+            }, delay)
+            return () => {
+                clearTimeout(handler)
+            }
+        }, [value, delay])
+        return debouncedValue
+    }
+    const debouncedSearchTerm = useDebounce(searchTerm, 1000)
 
     const inputRef = useRef()
-
-    const search = async (string) => {
-        const result = await index.search(string)
-        setHits(result.hits)
-    }
 
     const checkInput = () => {
         const { current } = inputRef
         if (current) {
             const { textContent } = current
-            search(textContent)
+            setSearchTerm(textContent)
             return textContent
         }
         return false
@@ -39,12 +54,28 @@ const SearchComponent = () => {
         }
     }
 
+    useEffect(() => {
+        if (debouncedSearchTerm) {
+            setIsSearch(false)
+            setHasContent(true)
+            ;(async () => {
+                const result = await index.search(debouncedSearchTerm)
+                setHits(result.hits)
+            })()
+        } else {
+            setHits([])
+            setIsSearch(false)
+            setHasContent(false)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [debouncedSearchTerm])
+
     return (
         <>
             <div className="position-relative ms-2 mt-2 mb-2">
-                <div className="position-relative border rounded-pill p-2">
+                <div className="position-relative border rounded-pill">
                     <div
-                        className={`text-muted position-absolute ms-2 ${
+                        className={`text-muted position-absolute ms-3 me-5 p-2 ${
                             hasContent ? 'd-none' : ''
                         }`}
                     >
@@ -58,23 +89,35 @@ const SearchComponent = () => {
                         onSelect={handleInputChange}
                         onBlur={handleInputChange}
                         onFocus={handleInputChange}
+                        className="ms-3 me-5 p-2 border-end"
                     />
                     <SearchIcon classes="position-absolute top-50 end-0 translate-middle-y me-3" />
                 </div>
-                {hasContent && hits.length > 0 && (
+                {isSearch ? (
                     <div
-                        className="position-absolute w-100 bg-white border rounded mt-1 p-3"
+                        className="position-absolute w-100 bg-white border rounded mt-1 p-3 text-muted"
                         style={{ zIndex: 99 }}
                     >
-                        {hits.map((hit) => {
-                            const { objectID, username } = hit
-                            return (
-                                <div key={`search-hit-${objectID}`}>
-                                    <Link to={`/${username}`}>@{username}</Link>
-                                </div>
-                            )
-                        })}
+                        <Spinner variant="primary" animation="border" role="status" size="sm" />{' '}
+                        looking
                     </div>
+                ) : (
+                    hasContent &&
+                    hits.length > 0 && (
+                        <div
+                            className="position-absolute w-100 bg-white border rounded mt-1 p-3"
+                            style={{ zIndex: 99 }}
+                        >
+                            {hits.map((hit) => {
+                                const { objectID, username } = hit
+                                return (
+                                    <div key={`search-hit-${objectID}`}>
+                                        <Link to={`/${username}`}>@{username}</Link>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
                 )}
             </div>
         </>
